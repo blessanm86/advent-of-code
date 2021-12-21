17d0b5aedc0925c849df77a48a8d6084d4b24a50
Blessan Mathew <blessanm86@gmail.com>
2021-12-21 23:10:36 +0100

    Part 1 works

--- 2020/day-1/part-2/rust/src/main.rs
+++ 2020/day-1/part-2/rust/src/main.rs
@@ -1,51 +1,73 @@
 use std::fs;
 
-fn parse_input(input: &String) -> Vec<i32> {
-    let mut parsed_input: Vec<i32> = Vec::new();
-
-    for (_, num) in input.lines().enumerate() {
-        parsed_input.push(num.trim().parse::<i32>().unwrap());
-    }
-
-    parsed_input
+fn parse_input(input: &str) -> Vec<u32> {
+    input
+        .lines()
+        .map(|str| str.trim().parse::<u32>().unwrap())
+        .collect::<Vec<u32>>()
 }
 
-fn solve(expenses: &Vec<i32>, count: i32, total: i32, sum: i32) -> Vec<i32> {
-    //JS code. Need to learn more about closures and iterators in rust.
+// fn solve(expenses: &Vec<u32>, count: u32, total: u32, sum: u32) -> Option<&Vec<u32>> {
+//     if count == 1 {
+//         let result = expenses.into_iter().position(|x| x + sum == total);
+//         let empty: Vec<u32> = vec![];
+//         return match result {
+//             Some(x) => Some(slice::from_ref(&expenses[x])),
+//             None => Some(&empty),
+//         };
+//     }
+//
+//     for i in 0..expenses.len() {
+//         println!("{:?}", expenses[i]);
+//         let num = solve(&expenses[i + 1..], count - 1, total, expenses[i] + sum).unwrap();
+//
+//         if num.len() > 0 {
+//             let mut nums: Vec<u32> = vec![expenses[i]];
+//             for &i in num {
+//                 nums.push(i);
+//             }
+//
+//             return Some(nums);
+//         }
+//     }
+//
+//     None
+// }
 
+fn calc(expenses: &[u32], count: u32, total: u32, sum: u32) -> Option<Vec<u32>> {
     if count == 1 {
-        let r = expenses.iter().find(|&exp| *exp + sum == total).unwrap();
-        let mut vec: Vec<i32> = Vec::new();
-        vec.push(r);
-        println!("{}", r);
-        return vec;
-    } else {
-        // for (let i = 0; i < expenses.length; i++) {
-        //     const slice = expenses.slice(i + 1);
-        //     const num = solve(slice, count - 1, total, expenses[i] + sum);
-        //     if (num) {
-        //         return [expenses[i], num].flat();
-        //     }
-        // }
-        // let mut i = 0;
-        // loop {
-        //     let slice: Vec<i32> = expenses[i..];
-        //     let num = solve(&slice, count - 1, total, expenses[i] + sum);
-        //     println!("{:?}", num);
-        // }
+        let result = expenses.into_iter().position(|x| x + sum == total);
+        return match result {
+            Some(x) => Some(vec![expenses[x]]),
+            None => Some(Vec::new()),
+        };
     }
 
-    return Vec::new();
+    for i in 0..expenses.len() {
+        let num = calc(&expenses[i + 1..], count - 1, total, expenses[i] + sum).unwrap();
+
+        if num.len() > 0 {
+            let mut nums: Vec<u32> = vec![expenses[i]];
+            for i in num {
+                nums.push(i);
+            }
+
+            return Some(nums);
+        }
+    }
+
+    None
 }
 
 fn main() {
-    // let test_input = fs::read_to_string("./src/test_input.txt").expect("Error reading the file");
-    // let input = fs::read_to_string("./src/input.txt").expect("Error reading the file");
-    //
-    // let parsed_test_input = parse_input(&String::from(test_input));
-    // assert_eq!(solve(&parsed_test_input, 2, 2020, 0), (1721, 299));
-    // solve(&parsed_test_input, 2, 2020, 0);
+    let test_input = fs::read_to_string("./src/test_input.txt").expect("Error reading the file");
+    let parsed_test_input = parse_input(&test_input);
+    assert_eq!(
+        calc(&parsed_test_input, 2, 2020, 0).unwrap(),
+        vec![1721, 299]
+    );
 
-    // let parsed_input = parse_input(&String::from(input));
-    // assert_eq!(get_pairs(&parsed_input, 2020), (618, 1402));
+    // let input = fs::read_to_string("./src/input.txt").expect("Error reading the file");
+    // let parsed_input = parse_input(&input);
+    // assert_eq!(calc(&parse_inputarsed_input, 3, 2020, 0).unwrap(), vec![1721, 299]);
 }
