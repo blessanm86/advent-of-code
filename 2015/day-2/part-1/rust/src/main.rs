02885b76ac4559a316edf8a3f241b74f207f1322
blessanm86 <blessanm86@gmail.com>
2021-12-27 23:19:36 +0100

    2015, d2, p1

--- 2015/day-2/part-1/rust/src/main.rs
+++ 2015/day-2/part-1/rust/src/main.rs
@@ -1,10 +1,10 @@
 use std::fs;
 
-fn parse_input(input: &String) -> Vec<Vec<&str>> {
-    let mut parsed_input: Vec<Vec<&str>> = Vec::new();
+fn parse_input(input: &String) -> Vec<Vec<u32>> {
+    let mut parsed_input: Vec<Vec<u32>> = Vec::new();
 
     for (_, num) in input.lines().enumerate() {
-        let dimensions: Vec<&str> = num.trim().split("x").collect();
+        let dimensions: Vec<u32> = num.trim().split("x").map(|x| x.parse().unwrap()).collect();
         parsed_input.push(dimensions);
     }
 
@@ -21,11 +21,19 @@ fn get_paper_area(length: u32, width: u32, height: u32) -> u32 {
     return 2 * top_area + 2 * width_area + 2 * length_area + min_area;
 }
 
+fn solve(dimens: Vec<Vec<u32>>) -> u32 {
+    dimens
+        .iter()
+        .map(|dim| get_paper_area(dim[0], dim[1], dim[2]))
+        .fold(0, |sum, x| sum + x)
+}
+
 fn main() {
     assert_eq!(get_paper_area(2, 3, 4), 58);
     assert_eq!(get_paper_area(1, 1, 10), 43);
 
     let puzzle_input =
         fs::read_to_string("./src/puzzle-input.txt").expect("Error reading the file");
-    parse_input(&puzzle_input);
+    let result = solve(parse_input(&puzzle_input));
+    println!("{}", result);
 }
